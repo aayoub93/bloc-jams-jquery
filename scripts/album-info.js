eec5303b2e20c475e8c5be8d18b3c98d848db353
aayoub93 <aayoub93@gmail.com>
2019-12-21 22:46:27 -0500

    checkpoint 15 album info work

---
+++ scripts/album-info.js
@@ -0,0 +1,7 @@
+//quick note: you don't need to use .ready() here since all the scripts are
+//declared towards the end of the HTML right above </body>
+
+{
+  $('#album-title').text(album.title);
+  $('img#album-cover-art').attr('src', album.albumArtUrl);
+}
